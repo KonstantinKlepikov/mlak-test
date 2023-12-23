d2670b9562bc7691bdcff6152314909681f8a298
Konstantin Klepikov <oformleno@gmail.com>
2023-12-23 17:35:41 +0200

    9, 10

--- src/task_10.py
+++ src/task_10.py
@@ -3,14 +3,24 @@
 Создайте простой веб-сервер (Flask, FastAPI). Напишите эндпойнт, который добавляет результат в гугл-таблицу и эндпойнт,
 который возвращает данные из гугл таблицы в виде JSON-объекта.
 
-Run with `uvicorn src.task_10:app --reload`
+Для связи с google scheet использована баблиотека gspread в режиме "для юзера".
+Это означает что не создавался бот, а использовался живой аккаунт.
+Чтобы реализовать этот функционал, пожалуйста пройдите аутентификацию
+для google по инструкции For End Users: Using OAuth Client ID:
+https://docs.gspread.org/en/latest/oauth2.html#enable-api-access
 
-Docs is available at url: http://127.0.0.1:8000/docs
+Запуск приложения: `uvicorn src.task_10:app --reload`
+
+Апи для теста руками доступно по адресу: http://127.0.0.1:8000/docs
+
+В качестве запроса - опрашивается фейковый апи, генерирующий свойства книг:
+https://fakerapi.it/api/v1/books?_quantity=1
 """
-from fastapi import FastAPI
-from pydantic import BaseModel, HttpUrl
-from datetime import date
+from fastapi import FastAPI, Depends
+from pydantic import BaseModel
 import requests
+import gspread
+from gspread import Spreadsheet
 
 
 EXT_ENPOINT = 'https://fakerapi.it/api/v1/books?_quantity=1'
@@ -18,8 +28,6 @@ EXT_ENPOINT = 'https://fakerapi.it/api/v1/books?_quantity=1'
 
 class BookIn(BaseModel):
     """Request external api scheme
-
-    https://fakerapi.it/api/v1/books?_quantity=1
     """
     id: int
     title: str
@@ -27,8 +35,8 @@ class BookIn(BaseModel):
     genre: str
     description: str
     isbn: int
-    image: HttpUrl
-    published: date
+    image: str
+    published: str
     publisher: str
 
     class Config:
@@ -47,26 +55,46 @@ class BookIn(BaseModel):
                     }
                 }
 
-
-class BookOut(BaseModel):
+class BooksOut(BaseModel):
     """Request google sheets api scheme
     """
     books: list[BookIn] = []
+
+
+def open_worksheet() -> Spreadsheet:
+    """Get gspread session"""
+    gc = gspread.oauth()
+    return gc.open("gsheet-test")
 
 
 app = FastAPI()
 
 
 @app.get("/book")
-def get_book() -> None:
+def get_book(gc: Spreadsheet = Depends(open_worksheet)) -> None:
+    """Get book from fake api and write it to google sheet
+    """
     q = requests.get(EXT_ENPOINT)
     if q.status_code == 200:
-        b = BookIn(**q.json()["data"][0])
+        b_in = BookIn(**q.json()["data"][0])
 
-    # send to google sheet
+        # send to google sheet
+        worksheet = gc.get_worksheet(0)
+        worksheet.append_row(
+            list(b_in.model_dump().values())
+                )
 
 
 @app.get("/books")
-def get_books() -> BookOut:
-    """"""
-
+def get_books(gc: Spreadsheet = Depends(open_worksheet)) -> BooksOut:
+    """Get all savwd in gs books
+    """
+    worksheet = gc.get_worksheet(0)
+    books = worksheet.get_all_values()
+    return BooksOut(books=[
+        BookIn(**{
+            k:v
+            for k,v
+            in zip(BookIn.model_fields.keys(), b)
+                }) for b in books
+            ])
